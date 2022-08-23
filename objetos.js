69f4aab3d157d85b82ccc16cceb245e90868716d
Araya <joaoaraya7@gmail.com>
2022-08-23 02:01:39 +0000

    primeiro commit

---
+++ objetos.js
@@ -0,0 +1,12 @@
+class Humano {
+    constructor(nome, dataNascimento, corPele)
+    {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.corPele = corPele;
+    }
+}
+
+let homem = new Humano('mario',1980, 'branco');
+
+console.log(homem);
